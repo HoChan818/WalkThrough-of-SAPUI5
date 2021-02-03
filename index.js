9e71675878c32e602482a85d792b88b7536488c8
HoChan818 <934706800@qq.com>
2021-02-03 07:54:03 +0000

    From step 1 to step 9 of WalkThrough Demo

---
+++ index.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/ui/core/ComponentContainer"],function(ComponentContainer){
+	"use strict";
+	
+	new ComponentContainer({
+			name: "sap.ui.demo",
+			async: true
+		}).placeAt("content");
+});
+	
