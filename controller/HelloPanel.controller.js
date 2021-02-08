2a4fda428356356e6793010775e43baa303d8bfa
HoChan818 <934706800@qq.com>
2021-02-08 09:55:52 +0000

    Step 16: Dialogs and Fragments

--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-	],function(Controller,MessageToast){
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+	],function(Controller, MessageToast, Fragment){
 	"use strict";
 	
 	return Controller.extend("sap.ui.demo.controller.HelloPanel",{
@@ -13,6 +14,27 @@ sap.ui.define([
 			
 			// show message
 			MessageToast.show(sMsg);
+		},
+		
+		onOpenDialog : function () {
+			var oView = this.getView();
+
+			// if the dialog in the fragment does not exist yet
+			if (!this.pDialog) {
+				this.pDialog = Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.view.HelloDialog"
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			} 
+			
+			// open Dialog
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
+			});
 		}
 	});
 });
